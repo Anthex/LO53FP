947df105b8622d99167beca7cae9c29d015d87d7
Anthex <achille.dantz@gmail.com>
2019-04-22 18:37:30 +0200

    update readme

--- README.mk
+++ README.mk
@@ -2,3 +2,9 @@
 Achille Dantz <achille@dantz.fr>
 
 This program aims to compute the approximate location of a mobile terminal for a known set of Wi-Fi accesspoints in an ideal 12x12m space for which we have 9 fingerprinting values (known RSSI for each accesspoint at a given location)
+
+Documentation : https://dantz.fr/LO53/structure.html
+
+Example Output : 
+
+![Screencap](https://dantz.fr/LO53/Capture.PNG)
